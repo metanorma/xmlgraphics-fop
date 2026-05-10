7393e4ff8eb2e3ae3b771bda7c98bf5c7b22f85f
Alexander Dyuzhev <dyuzhev@gmail.com>
2026-05-10 21:36:33 +0300

    Test added for FileAttachment annotation, metanorma/xmlgraphics-fop#47, metanorma/xmlgraphics-fop#24

--- fop-core/src/test/java/org/apache/fop/pdf/PDFAttachmentTestCase.java
+++ fop-core/src/test/java/org/apache/fop/pdf/PDFAttachmentTestCase.java
@@ -20,11 +20,20 @@ package org.apache.fop.pdf;
 
 import java.awt.Dimension;
 import java.awt.Rectangle;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
+import java.io.*;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.MimeConstants;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -38,6 +47,7 @@ import org.apache.fop.render.intermediate.extensions.URIAction;
 import org.apache.fop.render.pdf.PDFDocumentHandler;
 import org.apache.fop.render.pdf.extensions.PDFEmbeddedFileAttachment;
 
+import static org.apache.fop.pdf.PDFLinearizationTestCase.readObjs;
 
 
 public class PDFAttachmentTestCase {
@@ -106,4 +116,86 @@ public class PDFAttachmentTestCase {
         Assert.assertTrue(out.toString().contains("<<\n/S /JavaScript\n"
                 + "/JS (this.exportDataObject\\({cName:\"t\\(st\", nLaunch:2}\\);)\n>>"));
     }
+
+
+    @Test
+    public void testFileAttachmentAnnotation() throws Exception {
+        String fopxconf = "<fop version=\"1.0\">"
+                + "<accessibility>true</accessibility>"
+                + "<renderers><renderer mime=\"application/pdf\">"
+                + "<linearization>true</linearization>"
+                + "<filterList>\n"
+                + "    <value>null</value>\n"
+                + "  </filterList>\n"
+                + "</renderer></renderers></fop>";
+        String fo = "<fo:root xmlns:fo=\"http://www.w3.org/1999/XSL/Format\" xmlns:pdf=\"http://xmlgraphics.apache.org/fop/extensions/pdf\" xmlns:fox=\"http://xmlgraphics.apache.org/fop/extensions\">\n"
+                + "  <fo:layout-master-set>\n"
+                + "    <fo:simple-page-master master-name=\"simple\">\n"
+                + "      <fo:region-body/>\n"
+                + "    </fo:simple-page-master>\n"
+                + "  </fo:layout-master-set>\n"
+                + "<fo:declarations>\n" +
+                "    <pdf:catalog>\n" +
+                "      <pdf:dictionary key=\"ViewerPreferences\" type=\"normal\">\n" +
+                "        <pdf:boolean key=\"DisplayDocTitle\">true</pdf:boolean>\n" +
+                "      </pdf:dictionary>\n" +
+                "    </pdf:catalog>\n" +
+                "    <x:xmpmeta xmlns:x=\"adobe:ns:meta/\">\n" +
+                "      <rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\">\n" +
+                "        <rdf:Description xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:pdf=\"http://ns.adobe.com/pdf/1.3/\" rdf:about=\"\">\n" +
+                "          <dc:title>\n" +
+                "            <rdf:Alt>\n" +
+                "              <rdf:li xml:lang=\"x-default\">Document for test attachment</rdf:li>\n" +
+                "            </rdf:Alt>\n" +
+                "          </dc:title>\n" +
+                "          <dc:creator>\n" +
+                "            <rdf:Seq>\n" +
+                "              <rdf:li>Metanorma</rdf:li>\n" +
+                "            </rdf:Seq>\n" +
+                "          </dc:creator>\n" +
+                "          <pdf:Keywords>PDF, file attachment, test.</pdf:Keywords>\n" +
+                "        </rdf:Description>\n" +
+                "        <rdf:Description xmlns:xmp=\"http://ns.adobe.com/xap/1.0/\" rdf:about=\"\">\n" +
+                "          <xmp:CreatorTool/>\n" +
+                "        </rdf:Description>\n" +
+                "      </rdf:RDF>\n" +
+                "    </x:xmpmeta>\n" +
+                "    <pdf:embedded-file link-as-file-annotation=\"true\" filename=\"simple_text.txt\" src=\"data:application/octet-stream;base64,U2ltcGxlIHRleHQuDQo=\" description=\"File description\" afrelationship=\"AFR_Data\" volatile=\"true\"/>\n" +
+                "  </fo:declarations>"
+                + "  <fo:page-sequence master-reference=\"simple\">\n"
+                + "    <fo:flow flow-name=\"xsl-region-body\">\n"
+                + "      <fo:block>File: <fo:basic-link fox:alt-text=\"reference to Attachment\" external-destination=\"url(embedded-file:simple_text.txt)\" role=\"Annot\">reference to Attachment</fo:basic-link></fo:block>\n"
+                + "    </fo:flow>\n"
+                + "  </fo:page-sequence>\n"
+                + "</fo:root>\n";
+        FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI(),
+                new ByteArrayInputStream(fopxconf.getBytes()));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, fopFactory.newFOUserAgent(), out);
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        Source src = new StreamSource(new ByteArrayInputStream(fo.getBytes()));
+        Result res = new SAXResult(fop.getDefaultHandler());
+        transformer.transform(src, res);
+        checkPDF(out.toByteArray());
+    }
+
+    private void checkPDF(byte[] data) throws IOException {
+
+        InputStream is = new ByteArrayInputStream(data);
+        Map<String, StringBuilder> objs = readObjs(is);
+
+        String objects = objs.values().stream().collect(Collectors.joining(" "));
+
+        Assert.assertTrue(objects.contains("/Type /Annot /Subtype /FileAttachment\n" +
+                "/FS"));
+        Assert.assertTrue(objects.contains("/Contents (File description)\n" +
+                "/Name /Paperclip"));
+        Assert.assertTrue(objects.contains("/Type /Filespec\n" +
+                "  /F (simple_text.txt)\n" +
+                "  /UF (simple_text.txt)\n" +
+                "  /AFRelationship /AFR_Data"));
+        Assert.assertTrue(objects.contains("/Desc (File description)\n" +
+                "  /V true"));
+    }
+
 }
