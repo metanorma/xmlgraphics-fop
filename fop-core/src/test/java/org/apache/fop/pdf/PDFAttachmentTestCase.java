adbea6eae3cb4d6f1ed5a08f2eb0b9a1c97cff13
Alexander Dyuzhev <dyuzhev@gmail.com>
2026-05-17 19:41:03 +0300

    PDFAttachmentTestCase updated for /Contents in UTF-16BE, #71

--- fop-core/src/test/java/org/apache/fop/pdf/PDFAttachmentTestCase.java
+++ fop-core/src/test/java/org/apache/fop/pdf/PDFAttachmentTestCase.java
@@ -21,6 +21,8 @@ package org.apache.fop.pdf;
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -160,7 +162,7 @@ public class PDFAttachmentTestCase {
                 "        </rdf:Description>\n" +
                 "      </rdf:RDF>\n" +
                 "    </x:xmpmeta>\n" +
-                "    <pdf:embedded-file link-as-file-annotation=\"true\" filename=\"simple_text.txt\" src=\"data:application/octet-stream;base64,U2ltcGxlIHRleHQuDQo=\" description=\"File description\" afrelationship=\"AFR_Data\" volatile=\"true\"/>\n" +
+                "    <pdf:embedded-file link-as-file-annotation=\"true\" filename=\"simple_text.txt\" src=\"data:application/octet-stream;base64,U2ltcGxlIHRleHQuDQo=\" description=\"File “description”\" afrelationship=\"AFR_Data\" volatile=\"true\"/>\n" +
                 "  </fo:declarations>"
                 + "  <fo:page-sequence master-reference=\"simple\">\n"
                 + "    <fo:flow flow-name=\"xsl-region-body\">\n"
@@ -188,13 +190,19 @@ public class PDFAttachmentTestCase {
 
         Assert.assertTrue(objects.contains("/Type /Annot /Subtype /FileAttachment\n" +
                 "/FS"));
-        Assert.assertTrue(objects.contains("/Contents (File description)\n" +
+
+        byte[] bytesContents =  "File “description”".getBytes("UTF-16BE");
+
+        // "/Contents (" +
+        Assert.assertTrue(objects.contains(new String(bytesContents) + ")\n" +
                 "/Name /Paperclip"));
+
         Assert.assertTrue(objects.contains("/Type /Filespec\n" +
                 "  /F (simple_text.txt)\n" +
                 "  /UF (simple_text.txt)\n" +
                 "  /AFRelationship /AFR_Data"));
-        Assert.assertTrue(objects.contains("/Desc (File description)\n" +
+        //Assert.assertTrue(objects.contains("/Desc (File description)\n" +
+        Assert.assertTrue(objects.contains("/Desc <FEFF00460069006C00650020201C006400650073006300720069007000740069006F006E201D>\n" +
                 "  /V true"));
     }
 
