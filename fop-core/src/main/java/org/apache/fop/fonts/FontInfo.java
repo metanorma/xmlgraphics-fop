62772d12bc1c2d9fa391fe0f0c58b593a4173224
Alexander Dyuzhev <dyuzhev@gmail.com>
2026-05-21 22:07:18 +0300

    FontInfo restored to the original, #70

--- fop-core/src/main/java/org/apache/fop/fonts/FontInfo.java
+++ fop-core/src/main/java/org/apache/fop/fonts/FontInfo.java
@@ -26,14 +26,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
-import java.util.logging.Logger;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.fop.render.intermediate.EventProducingFilter;
-import org.metanorma.Constants;
-import org.metanorma.fop.SourceXMLDocument;
-import org.metanorma.utils.LoggerHelper;
 
 
 /**
@@ -49,8 +44,6 @@ public class FontInfo {
 
     /** logging instance */
     protected static final Log log = LogFactory.getLog(FontInfo.class);
-
-    protected static final Logger logger = Logger.getLogger(LoggerHelper.LOGGER_NAME);
 
     /** Map containing fonts that have been used */
     private Map<String, Typeface> usedFonts; //(String = font key)
@@ -70,10 +63,6 @@ public class FontInfo {
 
     /** Event listener for font events */
     private FontEventListener eventListener;
-
-    // for notifyFontReplacement in FontInfo
-    // https://github.com/metanorma/mn2pdf/issues/384#issuecomment-3809689570
-    private static List<String> replacedFonts = new ArrayList<>();
 
     /**
      * Main constructor
@@ -223,12 +212,7 @@ public class FontInfo {
 
         if (fontTriplet != null) {
             if (fontTriplet != startKey) {
-                // To do: https://github.com/metanorma/mn2pdf/issues/360
-                if (SourceXMLDocument.mainFont.equals(startKey.getName()) ||
-                        Constants.DEBUG ||
-                        !(SourceXMLDocument.mainAdditionalFonts.contains(startKey.getName()))) {
-                    notifyFontReplacement(startKey, fontTriplet);
-                }
+                notifyFontReplacement(startKey, fontTriplet);
             }
             return fontTriplet;
         } else {
@@ -455,24 +439,9 @@ public class FontInfo {
     }
 
     private void notifyFontReplacement(FontTriplet replacedKey, FontTriplet newKey) {
-        // commented due https://github.com/metanorma/mn2pdf/issues/392#issuecomment-3840144806
-        /*if (this.eventListener != null) {
+        if (this.eventListener != null) {
             this.eventListener.fontSubstituted(this, replacedKey, newKey);
-        } else {*/
-            // https://github.com/metanorma/mn2pdf/issues/384#issuecomment-3809689570
-            int currentPage =  EventProducingFilter.getCurrentPage() + 1;
-            String msg = "Page #" + currentPage + ": " + "Font \"" + replacedKey + "\" not found. Substituting with \"" + newKey+ "\".";
-            if (!replacedFonts.contains(msg)) {
-                /*int pageEventProducingFilter = EventProducingFilter.getCurrentPage();
-                if (currentPage != pageEventProducingFilter) {
-                    currentPage = pageEventProducingFilter;
-                    logger.severe("Page #" + currentPage);
-                }
-                currentPage = pageEventProducingFilter;*/
-                logger.severe(msg);
-                replacedFonts.add(msg);
-            }
-        //}
+        }
     }
 
     /**
