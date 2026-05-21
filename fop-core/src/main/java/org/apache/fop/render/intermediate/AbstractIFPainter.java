14375f4eacf7fb14d21243a0bd2f9ca9d5ea057d
Alexander Dyuzhev <dyuzhev@gmail.com>
2026-05-22 00:14:38 +0300

    code reverted back to pass tests, #47

--- fop-core/src/main/java/org/apache/fop/render/intermediate/AbstractIFPainter.java
+++ fop-core/src/main/java/org/apache/fop/render/intermediate/AbstractIFPainter.java
@@ -41,7 +41,6 @@ import org.apache.xmlgraphics.image.loader.ImageFlavor;
 import org.apache.xmlgraphics.image.loader.ImageInfo;
 import org.apache.xmlgraphics.image.loader.ImageManager;
 import org.apache.xmlgraphics.image.loader.ImageSessionContext;
-import org.apache.xmlgraphics.image.loader.impl.imageio.ImageLoaderImageIO;
 import org.apache.xmlgraphics.image.loader.util.ImageUtil;
 
 import org.apache.fop.ResourceEventProducer;
@@ -161,7 +160,6 @@ public abstract class AbstractIFPainter<T extends IFDocumentHandler> implements 
 
         ImageFlavor[] flavors = imageHandlerRegistry.getSupportedFlavors(context, getImageForSupportedFlavors(info));
         info.getCustomObjects().put("warningincustomobject", true);
-        info.getCustomObjects().put(ImageLoaderImageIO.ICC_CONVERTER, manager.getRegistry().getIccConverter());
         org.apache.xmlgraphics.image.loader.Image img = manager.getImage(
                     info, flavors,
                     hints, sessionContext);
